b830f10a80e635ae0106f72921021eab98d0f575
Edward Mack <ed@edwardmack.com>
2025-11-21 13:15:57 -0500

    update app api

--- www/js/api.js
+++ www/js/api.js
@@ -1,6 +1,6 @@
 // API Client for Voyage Logbook
 // Update this URL when deploying to production
-const API_URL = 'https://voyage-logbook.onrender.com';
+const API_URL = 'www/js/api.js';
 
 // Get stored auth token
 function getToken() {
